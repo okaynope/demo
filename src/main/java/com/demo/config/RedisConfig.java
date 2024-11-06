a299f2117586f35a7bcfdf5c059ac843e6c158e8
huangshan <3328568099@qq.com>
2024-11-06 14:15:01 +0800

    test1

---
+++ src/main/java/com/demo/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean(name="redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        System.out.println("\n Redis开启序列化");
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        //key序列化方式
+        template.setKeySerializer(redisSerializer);
+        //key haspmap序列化
+        template.setHashKeySerializer(redisSerializer);
+//        //value序列化
+//        template.setValueSerializer(redisSerializer);
+//        //value hashmap序列化
+//        template.setHashValueSerializer(redisSerializer);
+        template.setConnectionFactory(factory);
+        return template;
+    }
+}
